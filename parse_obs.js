7c1286aa9332563895e4639a484b33ad3db843a6
Davi Almeida <davi.almeida@iebtinnovation.com>
2026-07-30 15:31:47 -0300

    limpando repositorio

--- parse_obs.js
+++
@@ -1,7 +0,0 @@
-const xlsx = require('xlsx');
-const workbook = xlsx.readFile('observacoes_status.xlsx');
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-const data = xlsx.utils.sheet_to_json(sheet);
-console.log("Columns:", Object.keys(data[0] || {}));
-console.log("First 3 rows:", data.slice(0, 3));
