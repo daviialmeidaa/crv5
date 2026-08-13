4b41922974ee61fdddf9bfe4567a3c83638f6afa
Davi Almeida <davi.almeida@iebtinnovation.com>
2026-08-13 18:55:46 -0300

    Fix migration script role object reference

--- scripts/migrate_roles.js
+++ scripts/migrate_roles.js
@@ -1,5 +1,15 @@
 const pgPool = require('../db/pgConnection');
-const { rolePermissions } = require('../middleware/rbac');
+const rolePermissions = {
+    'ADMIN': { canViewCR: true, canViewLC: true, canViewUsers: true, canManageUsers: true, canViewClientes: true, canCreateUsers: true, canManageRoles: true, canResetHeroku: true },
+    'CR1': { canViewCR: true, canViewLC: false, canViewUsers: false, canManageUsers: false, canViewClientes: false, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'CR2': { canViewCR: true, canViewLC: false, canViewUsers: true, canManageUsers: false, canViewClientes: true, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'CR3': { canViewCR: true, canViewLC: false, canViewUsers: true, canManageUsers: false, canViewClientes: true, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'CR4': { canViewCR: true, canViewLC: false, canViewUsers: true, canManageUsers: false, canViewClientes: true, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'LC1': { canViewCR: false, canViewLC: true, canViewUsers: false, canManageUsers: false, canViewClientes: false, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'LC2': { canViewCR: false, canViewLC: true, canViewUsers: true, canManageUsers: false, canViewClientes: false, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'LC3': { canViewCR: false, canViewLC: true, canViewUsers: true, canManageUsers: false, canViewClientes: true, canCreateUsers: false, canManageRoles: false, canResetHeroku: false },
+    'LC4': { canViewCR: false, canViewLC: true, canViewUsers: true, canManageUsers: false, canViewClientes: true, canCreateUsers: false, canManageRoles: false, canResetHeroku: false }
+};
 
 async function migrateRoles() {
     try {
