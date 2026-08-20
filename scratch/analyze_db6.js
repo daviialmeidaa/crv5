6e66802989d5b8340546d15aba000f450238cc9a
Davi Almeida <davi.almeida@iebtinnovation.com>
2026-08-19 21:40:34 -0300

    chore: remove temporary scratch files and text dumps

--- scratch/analyze_db6.js
+++
@@ -1,7 +0,0 @@
-const pgPool = require('../db/pgConnection');
-async function run() {
-    const res = await pgPool.query('SELECT empresa, COUNT(*) as qtd FROM opme.contratos GROUP BY empresa');
-    console.log(res.rows);
-    process.exit(0);
-}
-run();
--- scratch/analyze_db7.js
+++
@@ -1,7 +0,0 @@
-const pgPool = require('../db/pgConnection');
-async function run() {
-    const res = await pgPool.query('SELECT id, cod_bio, produto FROM opme.cirurgias WHERE cod_bio IS NOT NULL LIMIT 5');
-    console.log(res.rows);
-    process.exit(0);
-}
-run();
